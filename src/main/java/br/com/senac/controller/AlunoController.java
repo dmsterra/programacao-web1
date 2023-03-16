49b226b5e2514b11e4aae347a01e693dc7cef4a1
ADS 2021.2N <36124872021.2N@senacrjedu.local>
2023-03-16 20:24:04 -0300

    segundo commit

--- src/main/java/br/com/senac/controller/AlunoController.java
+++ src/main/java/br/com/senac/controller/AlunoController.java
@@ -1,11 +1,15 @@
 package br.com.senac.controller;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.senac.entity.Aluno;
 import br.com.senac.service.AlunoService;
 
 @Controller
@@ -22,5 +26,43 @@ public class AlunoController {
 		return mv;
 		
 	}
+	
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrarAluno() {
+		ModelAndView mv = new ModelAndView("aluno/cadastraAluno");
+		mv.addObject("aluno", new Aluno());
+		return mv;
+	}
+	
+	@PostMapping("/salvar")
+	public ModelAndView salvarAluno(Aluno aluno) {
+		alunoService.salvar(aluno);
+		return listaTodosAlunos();
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alterarAluno(@PathVariable("id") Integer idAluno) throws ObjectNotFoundException {
+		ModelAndView mv = new ModelAndView("aluno/alteraAluno");
+		mv.addObject("aluno", alunoService.buscaPorID(idAluno));
+		return mv;
+	}
+	
+	@PostMapping("/alterar")
+	public ModelAndView alterar(Aluno alunoAlterado) throws ObjectNotFoundException {
+		alunoService.salvarAlteração(alunoAlterado);
+		return listaTodosAlunos();
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public ModelAndView excluirAluno(@PathVariable("id") Integer id) {
+		alunoService.excluir(id);
+		return listaTodosAlunos();
+	}
+	
+
+	
+	
+	
+	
 
 }
--- src/main/java/br/com/senac/service/AlunoService.java
+++ src/main/java/br/com/senac/service/AlunoService.java
@@ -1,7 +1,9 @@
 package br.com.senac.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +22,22 @@ public class AlunoService {
 	
 	public Aluno salvar(Aluno aluno){
 		return repoAluno.save(aluno);
+	}
+	
+	public Aluno buscaPorID(Integer id) throws ObjectNotFoundException{
+		Optional<Aluno> aluno = repoAluno.findById(id);
+		return aluno.orElseThrow(() -> new ObjectNotFoundException(aluno, "Aluno nao encontrado. Id: " + id));
+	}
+	
+	public Aluno salvarAlteração(Aluno alunoAlterado) throws ObjectNotFoundException {
+		Aluno aluno = buscaPorID(alunoAlterado.getId());
+		aluno.setId(alunoAlterado.getId());
+		aluno.setNome(alunoAlterado.getNome());
+		return salvar(aluno);
+	}
+	
+	public void excluir(Integer id) {
+		repoAluno.deleteById(id);
 	}
 
 }
